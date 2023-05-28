638de239cd8c413c93d71674bc0ed3805121eadb
Hassan <hassanch5209@gmail.com>
2023-05-28 22:25:54 +0500

    made changes in explore and table section

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -5,7 +5,7 @@ import img1 from "./icons/nft1.png"
 export default function Card() {
     return (
         <>
-            <div className={`col-md-5 px-md-3 py-md-2 ${style.yellowBorder} ${style.cardColor} ${style.card}`} >
+            <div className={`col-md-3 px-md-3 py-md-2 ${style.yellowBorder} ${style.cardColor} ${style.card}`}>
                 <div className={`row ${style.yellowBorder}`}>
                     <div className={`col-12 `}>
                         <img src={img1} alt="" className={` ${style.nft}`} />
@@ -16,7 +16,7 @@ export default function Card() {
                         <p className={`${style.greyColor} ${style.nftDesc}`}>Lorem ipsum dolor sit amet,  dlkss consetetur It is a long established fact</p>
                     </div>
 
-                    <div className={`col-12  mt-md-5`}>
+                    <div className={`col-12 mt-md-4`}>
                         <div className={`row `}>
                             <div className={`col-3 ${style.redBorder}`}>
                                 <p className={`${style.greyColor}`}> Copies </p>
@@ -24,7 +24,7 @@ export default function Card() {
                             </div>
 
                             <div className={`col-1 ${style.redBorder}`}>
-                                <div style={{height: '100%', backgroundColor: `${style.greyColor}`, width: '1px'}}></div>
+                                <div style={{height: '85%', backgroundColor: 'rgba(255, 255, 255, 0.65)', width: '1px'}}></div>
                             </div>
 
                             <div className={`col-3 ${style.redBorder}`}>
--- frontend/src/components/pages/Homepage.jsx
+++ frontend/src/components/pages/Homepage.jsx
@@ -4,6 +4,8 @@ import Footer from '../Footer'
 import style from '../../stylesheets/homepage.module.css'
 import welcomeImg from '../icons/welcome.png'
 import Card from '../Card'
+import Carousel from 'react-material-ui-carousel'
+import { Paper, Button } from '@mui/material'
 
 export default function Homepage() {
     return (
@@ -54,23 +56,128 @@ export default function Homepage() {
             </section>
 
             {/* explore section */}
-            <div>
-                <section className={`container my-md-5`}>
-                    <div className={`row ${style.yellowBorder}`}>
-                        <div className={`col-md-4 ${style.redBorder} `}>
-
+            <section className={`container-fluid mt-md-5 ${style.exploreSection}`}>
+                <div className={`row ${style.yellowBorder} align-items-center`}>
+                    <div className={`col-md-3 mx-auto ${style.redBorder} `}>
+                        <div>
+                            <h1>
+                                Explore NFTs <br />On Our <br /> Marketplace
+                            </h1>
                         </div>
 
-                        <div className={`col-md-7 ${style.blueBorder} `}>
-                            
-                            <div className={`row ${style.redBorder}`}>
-                                <Card></Card>
-                            </div>
+                        <div className={`py-md-3`}>
+                            <h6 style={{ fontSize: '20px', fontWeight: 'lighter' }}>
+                                Lorem ipsum dolor sit amet,
+                                cons <br />etetur sadipscing elitr, sed
+                            </h6>
+                        </div>
 
+                        <div className={`m-0 p-0`}>
+                            <button className={`btn px-md-5 py-md-2 ${style.btnExplorePlateform}`}>Explore Plateform</button>
                         </div>
                     </div>
-                </section>
-            </div>
+
+                    <div className={`col-md-8 ms-3 ${style.blueBorder} `}>
+
+                        <div className={`row ${style.redBorder} p-0 m-0`}>
+                            {/* <Card></Card>
+                                <Card></Card>
+                                <Card></Card> */}
+
+                            <Carousel autoPlay={false} animation={'slide'} indicators={false} swipe={true} cycleNavigation={false} navButtonsAlwaysVisible={true}>
+                                <div className={`row ${style.redBorder} p-0 m-0`}>
+                                    <Card></Card>
+                                    <Card></Card>
+                                    <Card></Card>
+                                </div>
+
+                                <div className={`row ${style.redBorder} p-0 m-0`}>
+                                    <Card></Card>
+                                </div>
+
+
+
+
+                            </Carousel>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+
+
+            {/* section */}
+
+            <section className={`container-fluid mt-md-5 ${style.tableSection}`}>
+                <div className={`row ${style.yellowBorder}`}>
+                    <div className={`col-md-5 ${style.redBorder} `}>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">First</th>
+                                    <th scope="col">Last</th>
+                                    <th scope="col">Handle</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">2</th>
+                                    <td>Jacob</td>
+                                    <td>Thornton</td>
+                                    <td>@fat</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">3</th>
+                                    <td>Larry</td>
+                                    <td>the Bird</td>
+                                    <td>@twitter</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <div className={`col-md-5 ${style.redBorder} `}>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">First</th>
+                                    <th scope="col">Last</th>
+                                    <th scope="col">Handle</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">2</th>
+                                    <td>Jacob</td>
+                                    <td>Thornton</td>
+                                    <td>@fat</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">3</th>
+                                    <td>Larry</td>
+                                    <td>the Bird</td>
+                                    <td>@twitter</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </section>
+
 
             <Footer />
         </>
