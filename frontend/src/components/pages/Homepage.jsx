e216e5c50b9b5596cda56413fccf3cfe3a3488c3
hananbabar98 <60232569+hananbabar98@users.noreply.github.com>
2023-06-03 17:07:53 +0500

    added table section and removed UI bugs

--- frontend/src/components/pages/Homepage.jsx
+++ frontend/src/components/pages/Homepage.jsx
@@ -3,9 +3,27 @@ import Navbar from '../Navbar'
 import Footer from '../Footer'
 import style from '../../stylesheets/homepage.module.css'
 import welcomeImg from '../icons/welcome.png'
+import profileIcon1 from '../icons/profileIcon3.png'
+import profileIcon2 from '../icons/profileIcon2.png'
+import profileIcon3 from '../icons/profileIcon1.png'
 import Card from '../Card'
 import Carousel from 'react-material-ui-carousel'
 import { Paper, Button } from '@mui/material'
+
+
+function PriceCol(props) {
+    return (
+        <>
+            <p style={{ fontSize: '21px', fontWeight: 'bold' }}>
+                24.4
+                <br />
+                <span className={`${props.color}`} style={{ fontSize: '12px', lineHeight: '1px' }}>
+                    +13.23%
+                </span>
+            </p>
+        </>
+    )
+}
 
 export default function Homepage() {
     return (
@@ -18,8 +36,8 @@ export default function Homepage() {
 
                     <div className={`col-md-4 mt-5 pt-5 ${style.yellowBorder} `}>
                         <div>
-                            <h1>
-                                Explore, collect,
+                            <h1 style={{fontSize: '60px'}}>
+                                Explore, collect,<br />
                                 and sell <span className={`${style.gradientText} `}>NFTs</span>
                             </h1>
                         </div>
@@ -56,7 +74,7 @@ export default function Homepage() {
             </section>
 
             {/* explore section */}
-            <section className={`container-fluid mt-md-5 ${style.exploreSection}`}>
+            <section className={`container-fluid py-md-5 ${style.exploreSection}`}>
                 <div className={`row ${style.yellowBorder} align-items-center`}>
                     <div className={`col-md-3 mx-auto ${style.redBorder} `}>
                         <div>
@@ -105,39 +123,79 @@ export default function Homepage() {
                 </div>
             </section>
 
-
             {/*table section */}
+            <section className={`container-fluid ${style.tableSection}`}>
+                <div className={`row ${style.yellowBorder} justify-content-evenly`}>
+                    <div className={`col-12 ${style.redBorder} my-md-5`}>
+                        <h1 style={{ textAlign: 'center', fontWeight: 'bold', fontSize: '50px', letterSpacing: '2px' }}>Top Collections Today</h1>
+                    </div>
 
-            <section className={`container-fluid mt-md-5 ${style.tableSection}`}>
-                <div className={`row ${style.yellowBorder}`}>
                     <div className={`col-md-5 ${style.redBorder} `}>
                         <table class="table">
                             <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">First</th>
-                                    <th scope="col">Last</th>
-                                    <th scope="col">Handle</th>
+                                <tr style={{ borderTop: '1px solid white' }}>
+                                    <th className={`${style.redBorder}`} scope="col"><div className={`py-md-3`}>Collections</div></th>
+                                    <th scope="col"><div className={`py-md-3`}>Price</div></th>
+                                    <th scope="col"><div className={`py-md-3`}>24h</div></th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
+                                <tr >
+                                    <td className={`${style.yellowBorder}`} style={{ verticalAlign: "top" }}>
+                                        <div className={`${style.redBorder} mt-md-3`} style={{ display: "flex", justifyContent: "left", alignItems: "center" }}>
+                                            <img src={profileIcon1} alt="" srcset="" /> {'\u00A0'} {'\u00A0'} <span style={{ fontSize: '20px', fontWeight: 'bold' }}>Perdgy Penguizs</span>
+                                        </div>
+                                    </td>
+
+                                    <td className={`${style.yellowBorder}`} style={{ verticalAlign: "middle" }}>
+                                        <div className={`${style.redBorder}`} style={{ display: "flex", justifyContent: "left", alignItems: "center", height: "100%" }}>
+                                            <PriceCol color={style.colorGreen} />
+                                        </div>
+                                    </td>
+
+                                    <td className={`${style.yellowBorder}`} style={{ verticalAlign: "middle" }}>
+                                        <div className={`${style.redBorder}`} style={{ display: "flex", justifyContent: "left", alignItems: "center", height: "100%" }}>
+                                            <PriceCol color={style.colorRed} />
+                                        </div>
+                                    </td>
                                 </tr>
-                                <tr>
-                                    <th scope="row">2</th>
-                                    <td>Jacob</td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
+                                <tr >
+                                    <td className={`${style.yellowBorder}`} style={{ verticalAlign: "top" }}>
+                                        <div className={`${style.redBorder} mt-md-3`} style={{ display: "flex", justifyContent: "left", alignItems: "center" }}>
+                                            <img src={profileIcon2} alt="" srcset="" /> {'\u00A0'} {'\u00A0'} <span style={{ fontSize: '20px', fontWeight: 'bold' }}>Azuki</span>
+                                        </div>
+                                    </td>
+
+                                    <td className={`${style.yellowBorder}`} style={{ verticalAlign: "middle" }}>
+                                        <div className={`${style.redBorder}`} style={{ display: "flex", justifyContent: "left", alignItems: "center", height: "100%" }}>
+                                            <PriceCol color={style.colorRed} />
+                                        </div>
+                                    </td>
+
+                                    <td className={`${style.yellowBorder}`} style={{ verticalAlign: "middle" }}>
+                                        <div className={`${style.redBorder}`} style={{ display: "flex", justifyContent: "left", alignItems: "center", height: "100%" }}>
+                                            <PriceCol color={style.colorRed} />
+                                        </div>
+                                    </td>
                                 </tr>
-                                <tr>
-                                    <th scope="row">3</th>
-                                    <td>Larry</td>
-                                    <td>the Bird</td>
-                                    <td>@twitter</td>
+                                <tr >
+                                    <td className={`${style.yellowBorder}`} style={{ verticalAlign: "top" }}>
+                                        <div className={`${style.redBorder} mt-md-3`} style={{ display: "flex", justifyContent: "left", alignItems: "center" }}>
+                                            <img src={profileIcon3} alt="" srcset="" /> {'\u00A0'} {'\u00A0'} <span style={{ fontSize: '20px', fontWeight: 'bold' }}>Otherdeed For Otherside</span>
+                                        </div>
+                                    </td>
+
+                                    <td className={`${style.yellowBorder}`} style={{ verticalAlign: "middle" }}>
+                                        <div className={`${style.redBorder}`} style={{ display: "flex", justifyContent: "left", alignItems: "center", height: "100%" }}>
+                                            <PriceCol color={style.colorRed} />
+                                        </div>
+                                    </td>
+
+                                    <td className={`${style.yellowBorder}`} style={{ verticalAlign: "middle" }}>
+                                        <div className={`${style.redBorder}`} style={{ display: "flex", justifyContent: "left", alignItems: "center", height: "100%" }}>
+                                            <PriceCol color={style.colorGreen} />
+                                        </div>
+                                    </td>
                                 </tr>
                             </tbody>
                         </table>
@@ -146,34 +204,78 @@ export default function Homepage() {
                     <div className={`col-md-5 ${style.redBorder} `}>
                         <table class="table">
                             <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">First</th>
-                                    <th scope="col">Last</th>
-                                    <th scope="col">Handle</th>
+                                <tr style={{ borderTop: '1px solid white' }}>
+                                    <th className={`${style.redBorder}`} scope="col"><div className={`py-md-3`}>Collections</div></th>
+                                    <th scope="col"><div className={`py-md-3`}>Price</div></th>
+                                    <th scope="col"><div className={`py-md-3`}>24h</div></th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
+                                <tr >
+                                    <td className={`${style.yellowBorder}`} style={{ verticalAlign: "top" }}>
+                                        <div className={`${style.redBorder} mt-md-2`} style={{ display: "flex", justifyContent: "left", alignItems: "center" }}>
+                                            <img src={profileIcon1} alt="" srcset="" /> {'\u00A0'} {'\u00A0'} <span style={{ fontSize: '20px', fontWeight: 'bold' }}>Perdgy Penguizs</span>
+                                        </div>
+                                    </td>
+
+                                    <td className={`${style.yellowBorder}`} style={{ verticalAlign: "middle" }}>
+                                        <div className={`${style.redBorder}`} style={{ display: "flex", justifyContent: "left", alignItems: "center", height: "100%" }}>
+                                            <PriceCol color={style.colorGreen} />
+                                        </div>
+                                    </td>
+
+                                    <td className={`${style.yellowBorder}`} style={{ verticalAlign: "middle" }}>
+                                        <div className={`${style.redBorder}`} style={{ display: "flex", justifyContent: "left", alignItems: "center", height: "100%" }}>
+                                            <PriceCol color={style.colorRed} />
+                                        </div>
+                                    </td>
                                 </tr>
-                                <tr>
-                                    <th scope="row">2</th>
-                                    <td>Jacob</td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
+                                <tr >
+                                    <td className={`${style.yellowBorder}`} style={{ verticalAlign: "top" }}>
+                                        <div className={`${style.redBorder} mt-md-2`} style={{ display: "flex", justifyContent: "left", alignItems: "center" }}>
+                                            <img src={profileIcon2} alt="" srcset="" /> {'\u00A0'} {'\u00A0'} <span style={{ fontSize: '20px', fontWeight: 'bold' }}>Azuki</span>
+                                        </div>
+                                    </td>
+
+                                    <td className={`${style.yellowBorder}`} style={{ verticalAlign: "middle" }}>
+                                        <div className={`${style.redBorder}`} style={{ display: "flex", justifyContent: "left", alignItems: "center", height: "100%" }}>
+                                            <PriceCol color={style.colorRed} />
+                                        </div>
+                                    </td>
+
+                                    <td className={`${style.yellowBorder}`} style={{ verticalAlign: "middle" }}>
+                                        <div className={`${style.redBorder}`} style={{ display: "flex", justifyContent: "left", alignItems: "center", height: "100%" }}>
+                                            <PriceCol color={style.colorRed} />
+                                        </div>
+                                    </td>
                                 </tr>
-                                <tr>
-                                    <th scope="row">3</th>
-                                    <td>Larry</td>
-                                    <td>the Bird</td>
-                                    <td>@twitter</td>
+                                <tr >
+                                    <td className={`${style.yellowBorder}`} style={{ verticalAlign: "top" }}>
+                                        <div className={`${style.redBorder} mt-md-2`} style={{ display: "flex", justifyContent: "left", alignItems: "center" }}>
+                                            <img src={profileIcon3} alt="" srcset="" /> {'\u00A0'} {'\u00A0'} <span style={{ fontSize: '20px', fontWeight: 'bold' }}>Otherdeed For Otherside</span>
+                                        </div>
+                                    </td>
+
+                                    <td className={`${style.yellowBorder}`} style={{ verticalAlign: "middle" }}>
+                                        <div className={`${style.redBorder}`} style={{ display: "flex", justifyContent: "left", alignItems: "center", height: "100%" }}>
+                                            <PriceCol color={style.colorRed} />
+                                        </div>
+                                    </td>
+
+                                    <td className={`${style.yellowBorder}`} style={{ verticalAlign: "middle" }}>
+                                        <div className={`${style.redBorder}`} style={{ display: "flex", justifyContent: "left", alignItems: "center", height: "100%" }}>
+                                            <PriceCol color={style.colorGreen} />
+                                        </div>
+                                    </td>
                                 </tr>
                             </tbody>
                         </table>
+                    </div>
+
+                    <div className={`w-100`}></div>
+
+                    <div className={`col-md-2 my-md-5 ${style.yellowBorder}`}>
+                        <button className={`btn px-md-5 py-md-2 ${style.btnSeeMore}`}>See More</button>
                     </div>
                 </div>
             </section>
