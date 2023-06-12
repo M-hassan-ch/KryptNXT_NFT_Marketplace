c9347a270b1c33489f2546d33f75b643b74321da
Hassan <hassanch5209@gmail.com>
2023-06-12 14:07:51 +0500

    designed choose account box

--- frontend/src/components/pages/CreateNft.jsx
+++ frontend/src/components/pages/CreateNft.jsx
@@ -11,31 +11,32 @@ export default function CreateNft() {
 
             <section>
                 <div className={`container ${style.yellowBorder}`}>
-                    <div className={`row ${style.formBackground} py-5`}>
+                    <div className={`row ${style.formBackground} py-5 px-md-5`}>
                         <div className={`col-12 mt-md-4 ${style.redBorder}`} style={{ fontSize: "57px", fontWeight: 'bold' }}>
                             <h1>Create New NFT</h1>
                         </div>
 
-                        <div className={`col-12 `}>
+                        <div className={`col-12`}>
                             <div className={`row justify-content-between`}>
                                 <div className={`col-md-5 ${style.blueBorder}`}>
 
                                     <div className={`row ${style.redBorder}`}>
-                                        <div className={`col-12 ${style.yellowBorder}`}>
-                                            <h5 style={{ fontWeight: 'bold' }}> Choose Account</h5>
+                                        <div className={`col-12 mt-md-5 mb-md-2 ${style.yellowBorder}`}>
+                                            <p style={{ fontWeight: 'bold', fontSize:'26px', letterSpacing:'1px' }}> Choose Account</p>
                                         </div>
 
                                         <div className={`col-12 ${style.blueBorder}`}>
-                                            <div className={`row align-items-center`}>
-                                                <div className={`col-md-1 p-0 ms-md-4 ${style.redBorder}`}>
-                                                    <img src={polygonIcon} alt="" srcset="" style={{ height: '45px', width: '100%' }} />
+                                            <div className={`row align-items-center py-md-3 `} style = {{background: 'rgba(39, 3, 39, 0.67)', border:'1px solid #808080', borderRadius: '42px'}}>
+                                                <div className={`col-md-2 p-0 ms-md-4 ${style.redBorder}`}>
+                                                    <img className={`ms-3 ${style.blueBorder}`}  src={polygonIcon} alt="" srcset="" style={{ height: '55px', width: '55px' }} />
                                                 </div>
 
-                                                <div className={`col-md-3 ms-md-3 pt-md-2 ${style.redBorder}`}>
-                                                    <p style={{ fontWeight: 'bold' }}>
-                                                        0x7E14a......09e4 <br />
-                                                        <span style={{ fontSize: '12px' }} className={`${style.greyColor}`}>Polygon</span>
+                                                <div className={`col-md-5 ms-md-3 pt-md-2 ${style.redBorder}`}>
+                                                    <p className={`mb-1 ${style.blueBorder}`} style={{fontSize:'20px', letterSpacing:'1px'}}>
+                                                        0x7E14a......09e4 
                                                     </p>
+
+                                                    <p  style={{ fontSize: '12px', fontWeight: 'bold'  }} className={`mt-0 ${style.greyColor} ${style.blueBorder}`}>Polygon</p>
                                                 </div>
 
                                                 <div className={`col-md-3 pt-md-2 ms-md-auto ${style.redBorder}`}>
